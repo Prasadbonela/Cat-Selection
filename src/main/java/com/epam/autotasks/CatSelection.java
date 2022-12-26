6ae1e94392f818d699df81db262997ffcae4492f
Sofia Krylova <sofia_krylova@epam.com>
2022-12-26 14:49:00 +0100

    Initial

---
+++ src/main/java/com/epam/autotasks/CatSelection.java
@@ -0,0 +1,27 @@
+package com.epam.autotasks;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class CatSelection {
+
+    public List<Cat> getFirstNCatsSortedByComparator(List<Cat> cats, Comparator<Cat> comparator, int number) {
+        throw new UnsupportedOperationException();
+    }
+
+    public List<Cat> getWithoutFirstNCatsSortedByComparator(List<Cat> cats, Comparator<Cat> comparator, int number) {
+        throw new UnsupportedOperationException();
+    }
+
+    public List<Cat> getSmallCats(List<Cat> cats, int threshold) {
+        throw new UnsupportedOperationException();
+    }
+
+    public List<Cat> getTallCats(List<Cat> cats, int threshold) {
+        throw new UnsupportedOperationException();
+    }
+
+    public List<String> getUniqueNames(List<Cat> cats) {
+        throw new UnsupportedOperationException();
+    }
+}
